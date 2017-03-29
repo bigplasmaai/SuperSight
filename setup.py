0a77627a31282c17583d676a52d393720c5b7cf5
Camille Moatti <camille.moatti@gmail.com>
2017-03-29 20:06:09 +0200

    added MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@
 from setuptools import setup, find_packages
 
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='supersight',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='A static website generator for MatplotLib plots',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Camille Moatti',
+    author_email='camille.moatti@gmail.com',
+    url='https://github.com/CamilleMo/SuperSight',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
